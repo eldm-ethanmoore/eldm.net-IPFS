7576b43b689e01c0614c906ee74e74b5450f31e4
Ethan L. Moore <eldm2350@protonmail.com>
2021-10-16 18:26:06 -0500

    Update redirectSidebar.js

--- js/redirectSidebar.js
+++ js/redirectSidebar.js
@@ -1,15 +1,15 @@
 redirectEldmNet = function redirectEldmNet() {
-    location.href = "https://gateway.ipfs.io/ipfs/QmRAFkyPCesVV7BR8SksnsKXgDKQSTTPeM9QVQt9R4BR2n"
+    location.href = "index.html"
 }
 redirectAbout = function redirectAbout() {
-    location.href = "https://gateway.ipfs.io/ipfs/QmQewTB1SJzALi36gviE9JoZqjS9zmdBmfpo2GeKNfpFHF"
+    location.href = "AboutMe.html"
 }
 redirectBlog = function redirectBlog() {
-    location.href = "https://gateway.ipfs.io/ipfs/QmWzCjsPEmQ2Aop6DJCkMzZJrDy8hbixpYwMnhM3oEPPgn";
+    location.href = "Blog.html";
 }
 redirectProjects = function redirectProjects() {
-    location.href = "https://gateway.ipfs.io/ipfs/QmW1yDD1XZxETRauNvhRe9vXQJM1RCyk2f3tegSTZbbovj"
+    location.href = "Projects.html"
 }
 redirectContact = function redirectContact() {
-    location.href = "https://gateway.ipfs.io/ipfs/QmZyEixYjVbr5c2bPeDiGsULPSwzJLNdo2Jc4wmcBSXFPa"
+    location.href = "Contact.html"
 }
